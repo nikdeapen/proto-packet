aab1e0abef5ee0c152e26f8b593a694d66ad43f0
Nik Deapen <nik@deapen.com>
2025-02-03 10:36:57 -0800

    [validate] Added support for validating type tags.

--- proto-packet-validate/src/lib.rs
+++ proto-packet-validate/src/lib.rs
@@ -1,5 +1,7 @@
 pub use common::*;
 pub use error::*;
+pub use var::*;
 
 mod common;
 mod error;
+mod var;
---
+++ proto-packet-validate/src/var/type_tag.rs
@@ -0,0 +1,47 @@
+use colored::Colorize;
+use lex::{ParseContext, Token};
+
+use proto_packet_parse::TypeTagTree;
+use proto_packet_tree::{QualifiedName, TypeTag};
+
+use crate::error::{Error, ErrorInfo};
+use crate::InvalidTypeTagError::InvalidQualifiedName;
+
+#[derive(Debug)]
+pub enum InvalidTypeTagError<'a> {
+    InvalidQualifiedName {
+        name: Token<'a>,
+        error_message: String,
+    },
+}
+
+impl<'a> Error for InvalidTypeTagError<'a> {
+    fn info(&self, _file_name: &str, _context: ParseContext) -> ErrorInfo {
+        let code: &str = "V_TYPE_TAG";
+        match self {
+            InvalidQualifiedName {
+                name,
+                error_message,
+            } => ErrorInfo {
+                code,
+                header: format!("invalid named type: {}", name),
+                info: vec![error_message.normal()],
+            },
+        }
+    }
+}
+
+pub fn validate_type_tag<'a>(tree: &'a TypeTagTree) -> Result<TypeTag, InvalidTypeTagError<'a>> {
+    match tree {
+        TypeTagTree::Primitive { primitive, .. } => Ok(primitive.to_type_tag()),
+        TypeTagTree::Special { special, .. } => Ok(special.to_type_tag()),
+        TypeTagTree::Named { name } => {
+            let name: QualifiedName =
+                QualifiedName::new(name.value()).map_err(|e| InvalidQualifiedName {
+                    name: *name,
+                    error_message: e.to_string(),
+                })?;
+            Ok(TypeTag::from(name))
+        }
+    }
+}
