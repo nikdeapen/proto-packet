1acda3274d836407e289d3ba8e8aad966eb416ed
Nik Deapen <nik@deapen.com>
2025-08-25 06:46:56 -0700

    [compile/tree] Added support for messages.

--- compile/proto-packet-tree/src/common/mod.rs
+++ compile/proto-packet-tree/src/common/mod.rs
@@ -1,3 +1,5 @@
 pub use with_comments::*;
+pub use with_tag_number::*;
 
 mod with_comments;
+mod with_tag_number;
--- compile/proto-packet-tree/src/lib.rs
+++ compile/proto-packet-tree/src/lib.rs
@@ -1,9 +1,11 @@
 pub use common::*;
+pub use message::*;
 pub use naming::*;
 pub use r#struct::*;
 pub use var::*;
 
 mod common;
+mod message;
 mod naming;
 mod r#struct;
 mod var;
