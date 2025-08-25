96f50109e8be923a0a3c1836056c5a82045e4175
Nik Deapen <nik@deapen.com>
2025-08-25 06:47:30 -0700

    [compile/tree] Added support for enums.

--- compile/proto-packet-tree/src/lib.rs
+++ compile/proto-packet-tree/src/lib.rs
@@ -1,10 +1,12 @@
 pub use common::*;
 pub use message::*;
 pub use naming::*;
+pub use r#enum::*;
 pub use r#struct::*;
 pub use var::*;
 
 mod common;
+mod r#enum;
 mod message;
 mod naming;
 mod r#struct;
