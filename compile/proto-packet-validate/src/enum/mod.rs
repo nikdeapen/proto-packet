381a14495d8c39b1c935f3026979881b4f20e718
Nik Deapen <nik@deapen.com>
2025-08-26 08:52:02 -0700

    [compile/validate] Added support for enums.

---
+++ compile/proto-packet-validate/src/enum/mod.rs
@@ -0,0 +1,5 @@
+pub use enum_case::*;
+pub use r#enum::*;
+
+mod r#enum;
+mod enum_case;
--- compile/proto-packet-validate/src/lib.rs
+++ compile/proto-packet-validate/src/lib.rs
@@ -1,10 +1,12 @@
 pub use common::*;
 pub use error::*;
 pub use message::*;
+pub use r#enum::*;
 pub use r#struct::*;
 pub use var::*;
 
 mod common;
+mod r#enum;
 mod error;
 mod message;
 mod r#struct;
