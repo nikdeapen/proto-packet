1b6c449d248181b0b9009366ee5c1639996ed615
Nik Deapen <nik@deapen.com>
2025-08-26 08:53:48 -0700

    [compile/validate] Added support for imports.

--- compile/proto-packet-validate/src/lib.rs
+++ compile/proto-packet-validate/src/lib.rs
@@ -1,6 +1,7 @@
 pub use common::*;
 pub use error::*;
 pub use message::*;
+pub use project::*;
 pub use r#enum::*;
 pub use r#struct::*;
 pub use var::*;
@@ -10,6 +11,7 @@ mod common;
 mod r#enum;
 mod error;
 mod message;
+mod project;
 mod r#struct;
 mod var;
 mod variant;
---
+++ compile/proto-packet-validate/src/project/mod.rs
@@ -0,0 +1,3 @@
+pub use import::*;
+
+mod import;
