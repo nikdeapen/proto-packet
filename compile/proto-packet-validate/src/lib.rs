ef4ffa2c2f0df02224d80769762bfc3e8ac5335f
Nik Deapen <nik@deapen.com>
2025-08-26 08:52:36 -0700

    [compile/validate] Added support for variants.

--- compile/proto-packet-validate/src/lib.rs
+++ compile/proto-packet-validate/src/lib.rs
@@ -4,6 +4,7 @@ pub use message::*;
 pub use r#enum::*;
 pub use r#struct::*;
 pub use var::*;
+pub use variant::*;
 
 mod common;
 mod r#enum;
@@ -11,3 +12,4 @@ mod error;
 mod message;
 mod r#struct;
 mod var;
+mod variant;
