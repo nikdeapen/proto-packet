47313fea1df6ad7b08f6c21a06080ff383c78a72
Nik Deapen <nik@deapen.com>
2025-08-26 08:47:08 -0700

    [compile/validate] Added support for type tags.

---
+++ compile/proto-packet-validate/src/var/type_tag.rs
@@ -0,0 +1,46 @@
+use crate::error::{Error, ErrorInfo};
+use crate::InvalidTypeTagError::InvalidQualifiedName;
+use crate::V_TYPE_TAG;
+use lex::{ParseContext, Token};
+use proto_packet_parse::TypeTagTree;
+use proto_packet_tree::{QualifiedName, TypeTag};
+
+#[derive(Debug)]
+pub enum InvalidTypeTagError<'a> {
+    InvalidQualifiedName {
+        name: Token<'a>,
+        error_message: String,
+    },
+}
+
+impl<'a> Error for InvalidTypeTagError<'a> {
+    fn info(&self, file_name: &str, context: ParseContext) -> ErrorInfo {
+        match self {
+            InvalidQualifiedName {
+                name,
+                error_message,
+            } => V_TYPE_TAG.info("invalid qualified name").with_token_info(
+                file_name,
+                context,
+                *name,
+                error_message.as_str(),
+            ),
+        }
+    }
+}
+
+pub fn validate_type_tag<'a>(tree: &'a TypeTagTree) -> Result<TypeTag, InvalidTypeTagError<'a>> {
+    match tree {
+        TypeTagTree::Primitive { primitive, .. } => Ok(primitive.to_type_tag()),
+        TypeTagTree::Special { special, .. } => Ok(special.to_type_tag()),
+        TypeTagTree::Named { name } => {
+            let name: QualifiedName =
+                QualifiedName::new(name.value()).map_err(|e| InvalidQualifiedName {
+                    name: *name,
+                    error_message: e.to_string(),
+                })?;
+            Ok(TypeTag::from(name))
+        }
+        TypeTagTree::Slice { base } => Ok(validate_type_tag(base)?.to_slice()),
+    }
+}
